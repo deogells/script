6de37c535d7eb26b86a8f924df3a60283a68e23f
deogells <deogells.colares@gmail.com>
2023-05-24 17:07:54 -0300

    add script instalar chocolatey :D

--- initial.ps1
+++ initial.ps1
@@ -17,6 +17,7 @@ net localgroup "administradores" $env:USERNAME /del
 net localgroup "usuários" $env:USERNAME /add
 
 Write-Host "Instalando Chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 Write-Host "Instalando powershell 7"
 choco install powershell-core -y
