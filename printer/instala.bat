ee776e7515831fec2003e1e2d8a3e0c0ee87286e
deogells <deogells.colares@gmail.com>
2023-05-25 17:32:14 -0300

    add parametros

--- printer/instala.bat
+++ printer/instala.bat
@@ -1,8 +1,8 @@
 
 powershell Set-ExecutionPolicy bypass
 
-powershell %temp%\printer\add-printer.ps1
+powershell %temp%\printer\add-printer.ps1 -wait
  
-powershell %temp%\printer\printerFIN.ps1
-powershell %temp%\printer\PrinterCMP.ps1  
+powershell %temp%\printer\printerFIN.ps1 -wait
+powershell %temp%\printer\PrinterCMP.ps1  -wait
 
