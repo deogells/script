a1a83dde94d8bf8af90a863e4ddc02985b40d068
deogells <deogells.colares@gmail.com>
2023-05-26 11:00:41 -0300

    corrigindo fluxo do script

--- initial.bat
+++ initial.bat
@@ -1,9 +1,16 @@
-%temp%\printer\m478f.exe && pnputil.exe /add-driver %temp%\m478f\hpclC62A4_x64.inf /install
+%temp%\printer\m478f.exe 
 
-%temp%\printer\mfp4103.exe && pnputil.exe /add-driver %temp%\mfp4103\hplo03744_x64.inf /install
+timeout /t /NOBREAK 3
+pnputil.exe /add-driver %temp%\m478f\hpclC62A4_x64.inf /install 
 
-powershell Set-ExecutionPolicy bypass
+
+
+%temp%\printer\mfp4103.exe 
+
+timeout /t /NOBREAK 3
+
+pnputil.exe /add-driver %temp%\mfp4103\hplo03744_x64.inf /install
+
+powershell Set-ExecutionPolicy bypass 
 
 powershell "%temp%\initial.ps1"
-
-exit
