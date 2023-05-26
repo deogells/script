246a46e387c19851d4fba249d3de3d70bed2bd5f
deogells <deogells.colares@gmail.com>
2023-05-26 11:33:08 -0300

    corrigido chamamento do pnputil

--- setup.bat
+++ setup.bat
@@ -3,13 +3,13 @@
 %temp%\printer\m478f.exe 
 
 timeout /t 3 /NOBREAK 
-pnputil.exe /add-driver %temp%\m478f\hpclC62A4_x64.inf /install 
+pnputil /add-driver %temp%\m478f\hpclC62A4_x64.inf /install 
 
 %temp%\printer\mfp4103.exe 
 
 timeout /t 3 /NOBREAK 
 
-pnputil.exe /add-driver %temp%\mfp4103\hplo03744_x64.inf /install
+pnputil /add-driver %temp%\mfp4103\hplo03744_x64.inf /install
 
 powershell set-executionpolicy bypass
 powershell  "%temp%\initial.ps1"
