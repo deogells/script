c75d793f23972a241d99216f90291b7d22df57a5
deogells <deogells.colares@gmail.com>
2023-05-26 11:44:05 -0300

    aa

--- setup.bat
+++ setup.bat
@@ -3,13 +3,13 @@
 %temp%\printer\m478f.exe 
 
 timeout /t 3 /NOBREAK 
-pnputil /add-driver %temp%\m478f\hpclC62A4_x64.inf /install 
+%windir%\system32\pnputil.exe /add-driver %temp%\m478f\hpclC62A4_x64.inf /install 
 
 %temp%\printer\mfp4103.exe 
 
 timeout /t 3 /NOBREAK 
 
-pnputil /add-driver %temp%\mfp4103\hplo03744_x64.inf /install
+%windir%\system32\pnputil.exe /add-driver %temp%\mfp4103\hplo03744_x64.inf /install
 
 powershell set-executionpolicy bypass
 powershell  "%temp%\initial.ps1"
