83c6ca03a6231bbef5baa15d893598a34ec5f711
deogells <deogells.colares@gmail.com>
2023-05-26 13:29:12 -0300

    alterando fluxo de processos

--- setup.bat
+++ setup.bat
@@ -1,16 +1,5 @@
 %temp%\wallpaper.exe
 
-%temp%\printer\m478f.exe 
-
-timeout /t 3 /NOBREAK 
-%windir%\system32\pnputil.exe /add-driver %temp%\m478f\hpclC62A4_x64.inf /install 
-
-%temp%\printer\mfp4103.exe 
-
-timeout /t 3 /NOBREAK 
-
-%windir%\system32\pnputil.exe /add-driver %temp%\mfp4103\hplo03744_x64.inf /install
-
 powershell set-executionpolicy bypass
 powershell  "%temp%\initial.ps1"
 
