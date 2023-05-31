603e72b3333d9bcb91d983a8c6f33cb46ddad60d
deogells <deogells.colares@gmail.com>
2023-05-31 09:24:26 -0300

    corrigido script

--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
 %temp%\wallpaper.exe
+powershell set-itemproperty -path "HKLM:SYSTEM\CurrentControlSet\Control\Nls\CodePage" -name OEMCP -value "65001"
 
 powershell set-executionpolicy bypass
 powershell  "%temp%\initial.ps1"
