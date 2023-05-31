e017e2c1da286e1715db8f6fee45a41a0837d47b
deogells <deogells.colares@gmail.com>
2023-05-31 11:33:23 -0300

    a

--- setup.bat
+++ setup.bat
@@ -1,5 +1,13 @@
-%temp%\wallpaper.exe
 powershell set-itemproperty -path "HKLM:SYSTEM\CurrentControlSet\Control\Nls\CodePage" -name OEMCP -value "65001"
+%temp%\wallpaper.exe
+
+echo "Normalizando usuários"
+net user administrador /active:yes
+net user administrador 'M1r4nt3$2023'
+net localgroup "administradores" $env:USERNAME /del
+net localgroup "usuarios" $env:USERNAME /add
+
+
 
 powershell set-executionpolicy bypass
 powershell  "%temp%\initial.ps1"
