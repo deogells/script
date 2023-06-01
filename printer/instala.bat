591326fc378b5b094917ee8c202bc331e40bc11e
deogells <deogells.colares@gmail.com>
2023-06-01 11:23:39 -0300

    corrigido script

--- printer/instala.bat
+++ printer/instala.bat
@@ -1,7 +1,7 @@
 
 powershell Set-ExecutionPolicy bypass -Scope CurrentUser
 
-powershell Start-Process "$Env:SystemDrive\temp\printer\add-printer.exe"-Verb runAs
+powershell Start-Process "$Env:SystemDrive\temp\printer\add-printer.exe" -Verb runAs
 
 
  	
