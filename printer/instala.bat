954a02852529c7f4d7fd501c9a243fb1cfaf4b05
deogells <78437124+deogells@users.noreply.github.com>
2023-06-05 13:33:08 -0300

    att geral

--- printer/instala.bat
+++ printer/instala.bat
@@ -1,7 +1,5 @@
 
-powershell Set-ExecutionPolicy bypass -Scope CurrentUser
-
-powershell Start-Process "$Env:SystemDrive\temp\printer\add-printer.exe" -Verb runAs
+%SystemDrive%\temp\printer\add-printer.exe" 
 
 
  	
